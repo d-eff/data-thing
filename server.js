71ec92cf9c494beb7f69e26b23a377f67db6cba1
David Fernandez <david.fernandez@showtime.net>
2017-11-02 14:36:04 -0400

    Switched from Pug to ES6 template literals, because ughhhhhh

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 const express = require('express');
+const es6Renderer = require('express-es6-template-engine')
 const app = express();
+const path = require('path');
 
 let datas;
 
-app.set('view engine', 'pug');
+app.use(express.static('styles'));
+app.locals.basedir = path.join(__dirname, '');
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
-  res.render('userEvents', JSON.parse(datas[0]));
+  res.render('userEvents', {locals: JSON.parse(datas[0])});
 });
 
 fs.readFile('data/userEventsActivity.json', (err, data) => {
