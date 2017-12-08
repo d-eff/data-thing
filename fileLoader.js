d88ba621481f15d6f85776bce19b2f8744cc0408
David Fernandez <david.fernandez@showtime.net>
2017-12-08 16:46:54 -0500

    Hot file reloads

--- fileLoader.js
+++ fileLoader.js
@@ -2,7 +2,18 @@ const fs = require('fs');
 
 let datas = {};
 
-const loadFiles = (cb) => {
+const watch = (reloadSameFiles = false) => {
+  fs.watch('data', (event, filename) => {
+    if(event === 'change') {
+      let shortname = filename.replace('.json', '');
+      if(reloadSameFiles || !datas.hasOwnProperty(shortname)) {
+        loadFile(filename);
+      }
+    } 
+  });
+}
+
+const loadFiles = (cb = () => {}) => {
   fs.readdir('data', (err, files) => {
     if (err) {
       return console.error("error reading /data" + err);
@@ -10,31 +21,36 @@ const loadFiles = (cb) => {
     
     //run through them, load each one up
     files.forEach((filename) => {
-      let fullPath = 'data/' + filename
-      let shortname = filename.replace('.json', '');
-      fs.readFile(fullPath, (err, data) => {
-        if (err) {
-          console.error("error reading file" + filename + " " + err);
-        }
-
-        //the files come through as newline-delimited JSON, have to be split and parsed
-        datas[shortname] = data.toString().trim().split('\n').map(val => {
-          var item = {};
-          try {
-            item = JSON.parse(val);
-          } catch(e) {
-            console.log("problem parsing data at " + fullPath);
-          }
-          return item;
-        });
-      });
+      loadFile(filename);
     });
   
     cb();
   });
 };
 
+const loadFile = (filename) => {
+  let fullPath = 'data/' + filename
+  let shortname = filename.replace('.json', '');
+  fs.readFile(fullPath, (err, data) => {
+    if (err) {
+      console.error("error reading file" + filename + " " + err);
+    }
+
+    //the files come through as newline-delimited JSON, have to be split and parsed
+    datas[shortname] = data.toString().trim().split('\n').map(val => {
+      var item = {};
+      try {
+        item = JSON.parse(val);
+      } catch(e) {
+        console.log("problem parsing data at " + fullPath);
+      }
+      return item;
+    });
+  });
+}
+
 module.exports = {
   loadFiles: loadFiles,
+  watch: watch,
   data: datas
 };
